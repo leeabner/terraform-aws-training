69c7e1b7be7c9d6fe3622d1f7b5593a89d5f9200
BigonDis <lee.abner@cibmis.com>
2026-07-22 15:07:22 -0500

    added additional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,21 @@
 output "instance_id" {
-  description = "EC2 instance ID"
+  description = "Ubuntu instance ID"
   value       = aws_instance.ubuntu01.id
 }
 
 output "public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "Public IP of the Ubuntu instance"
   value       = aws_instance.ubuntu01.public_ip
+}
+
+output "instance_id" {
+  description = "Amazon Linux instance ID"
+  value       = aws_instance.amazon_linux01.id
+}
+
+output "public_ip" {
+  description = "Public IP of the Amazon Linux instance"
+  value       = aws_instance.amazon_linux01.public_ip
 }
 
 output "vpc_id" {
