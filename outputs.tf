866440d93dc4c0bbb772c750f5a048556c60b8a7
BigonDis <lee.abner@cibmis.com>
2026-07-23 12:59:07 -0500

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "instance_id" {
+output "instance_id_ubuntu" {
   description = "Ubuntu instance ID"
   value       = aws_instance.ubuntu01.id
 }
 
-output "public_ip" {
+output "public_ip_ubuntu" {
   description = "Public IP of the Ubuntu instance"
   value       = aws_instance.ubuntu01.public_ip
 }
 
-output "instance_id" {
+output "instance_id_amazon_linux" {
   description = "Amazon Linux instance ID"
   value       = aws_instance.amazon_linux01.id
 }
